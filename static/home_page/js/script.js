b572b97fda5446e6103d39ea8a2ea2861fc20174
Kevin Pan <kcpan@ucsd.edu>
2016-01-29 23:32:29 +0000

    working well

--- static/home_page/js/script.js
+++ static/home_page/js/script.js
@@ -6,26 +6,35 @@ $(document).ready(function() {
     nextArrow: null,
     mobileFirst: true
   });
+  $('.group').css('transform','translateY(-50%)');
 
   $('#groupcon').click(function() {
     if (click) {
       $('#groupcon').animate({
         height: '50%'
-      });
+      },750, 'easeOutBounce');
+      $('.group').animate({
+        top: '10%'
+      },750, 'easeOutBounce');
       click = 0;
+      $('html, body').animate({
+        scrollTop: $('#groupcon').offset().top
+      }, 750 , 'easeOutBounce');
+      $('.group').text('CREATE GROUP -');
     } else {
       $('#groupcon').animate({
         height: '10%'
-      });
+      },750 , 'easeOutBounce');
+      $('.group').animate({
+        top: '50%'
+      },750, 'easeOutBounce');
       click=1;
+      $('html, body').animate({
+        scrollTop: $('#fakepad').offset().top
+      }, 750, 'easeOutBounce');
+      $('.group').text('CREATE GROUP +');
     }
-    $('html, body').animate({
-  		scrollTop: $('#groupcon').offset().top
-		}, 500);
 
-    $('.fakepad').css({
-      'height': '10%'
-    });
   });
 
 

43e04aeda0ab0017e7b246ba64ec24dc89a97929
Derek <dxdttk@gmail.com>
2016-01-28 23:51:01 -0800

    animated groups

--- static/home_page/js/script.js
+++ static/home_page/js/script.js
@@ -4,6 +4,26 @@ $(document).ready(function() {
     autoplay: true,
     prevArrow: null,
     nextArrow: null,
-    mobileFirst: true
+    mobileFirst: true,
+    adaptiveHeight: true
+  });
+
+  $('#groupcon').click(function() {
+    var click = 1;
+    if (click) {
+      $('#groupcon').animate({
+        height: '54%'
+      });
+      click = 0;
+    } else {
+      $('#groupcon').animate({
+        height: '-=200px'
+      });
+      click=1;
+    }
+    $('html, body').animate({
+  		scrollTop: $('#groupcon').offset().top
+		}, 500);
+    $('#groupvert').removeClass('vertCenter')
   });
 });

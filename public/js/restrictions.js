6ebea3e4589f6c843f2bee60f4bedd0bd8f46d94
Derek <dxdttk@gmail.com>
2016-02-10 00:18:29 -0800

    updated form look

--- public/js/restrictions.js
+++ public/js/restrictions.js
@@ -1,20 +1,20 @@
 $(document).ready(function() {
     var max_fields      = 10; //maximum input boxes allowed
-    var wrapper         = $(".input_fields_wrap"); //Fields wrapper
-    var add_button      = $(".add_field_button"); //Add button ID
+    var wrapper         = $(".input-fields-wrap"); //Fields wrapper
+    var add_button      = $(".add-field-button"); //Add button ID
 
     var x = 1; //initlal text box count
-    $(".add_field_button").click(function(){ //on add input button click
+    $(".add-field-button").click(function(){ //on add input button click
         event.preventDefault();
         addField();
     });
 
-    $(wrapper).on("click",".add_field_button", function(){ //user click on remove text
+    $(wrapper).on("click",".add-field-button", function(){ //user click on remove text
         event.preventDefault();
         addField();
     })
 
-    $(wrapper).on("click",".remove_field", function(){ //user click on remove text
+    $(wrapper).on("click",".remove-field", function(){ //user click on remove text
         event.preventDefault();
         $(this).parent('div').remove();
         x--;
@@ -23,8 +23,8 @@ $(document).ready(function() {
     function addField() {
       if(x < max_fields){ //max input box allowed
           x++; //text box increment
-          $(".add_field_button").replaceWith('<button class="remove_field">Remove</button>');
-          $(wrapper).append('<div><input class="res-field" type="text" name="mytext[]"/><button class="add_field_button">Add</button></div>'); //add input box
+          $(".add-field-button").replaceWith('<button class="remove-field">Remove</button>');
+          $(wrapper).append('<div class="field-line"><span><input class="res-field" type="text" name="mytext[]"/></span><button class="add-field-button">Add</button></div>'); //add input box
       }
     }
 });

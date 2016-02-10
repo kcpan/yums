70d729e3bc42e287732d6812be1765e211d52439
Derek <dxdttk@gmail.com>
2016-02-09 20:03:46 -0800

    handlebared

---
+++ public/js/restrictions.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+    var next = 1;
+    $(".add-more").click(function(){
+        event.preventDefault();
+        var addto = "#field" + next;
+        var addRemove = "#field" + (next);
+        next = next + 1;
+        var newIn = '<input autocomplete="off" class="input form-control" id="field' + next + '" name="field' + next + '" type="text">';
+        var newInput = $(newIn);
+        var removeBtn = '<button id="remove' + (next - 1) + '" href="#" class="btn btn-danger remove-me res-btn" >-</button></div><div id="field">';
+        var removeButton = $(removeBtn);
+        $(addto).after(newInput);
+        $(addRemove).after(removeButton);
+        $("#field" + next).attr('data-source',$(addto).attr('data-source'));
+        $("#count").val(next);
+
+            $('.remove-me').click(function(){
+                event.preventDefault();
+                var fieldNum = this.id.charAt(this.id.length-1);
+                var fieldID = "#field" + fieldNum;
+                $(this).remove();
+                $(fieldID).remove();
+
+                return false;
+            });
+
+        return false;
+    });
+});

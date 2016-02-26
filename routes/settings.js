c38059c18c3e56844bb92b8d34c154491a6f77c0
Kevin Pan <kcpan@ucsd.edu>
2016-02-25 23:57:14 -0800

    orig

--- routes/settings.js
+++ routes/settings.js
@@ -1,8 +1,9 @@
 // Get all of our friend data
 var data = require('../data.json');
+var categories = require('../categories.json');
 
 exports.view = function(req, res){
 	console.log(data);
-	res.render('settings', data);
+	res.render('settings', data, categories);
 	//res.render('index');
 };

46d21b4dc07227dfb02446b6c6efc85813ac075c
Derek Tran <dxdttk@gmail.com>
2016-03-02 02:38:45 -0800

    added code for 1v3 Testing

--- routes/rand-result.js
+++ routes/rand-result.js
@@ -2,7 +2,24 @@
 var data = require('../data.json');
 
 exports.view = function(req, res){
+	/*var random_num = Math.random();
+	console.log(random_num);
+
+	if (random_num > 0.5) {
+	  data["three"] = false;
+	  res.render('rand-result', data);
+	} else {
+	  res.redirect('/rand-result3');
+	}*/
+
+	data["three"] = false;
 	console.log(data);
 	res.render('rand-result', data);
 	//res.render('index');
 };
+
+exports.viewThree = function(req, res){
+	data["three"] = true;
+	console.log(data);
+	res.render('rand-result', data);
+}

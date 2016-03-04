6c7834f696c17970db14ff67eac836b9b8b1f27f
Derek Tran <dxdttk@gmail.com>
2016-03-03 21:20:33 -0800

    fb edit?

--- public/js/facebook.js
+++ public/js/facebook.js
@@ -13,9 +13,11 @@ function initializePage(){
         console.log('Logged in.');
       }
       else {
+        /*
         FB.login(function(response) {
           statusChangeCallback(response);
-        }, {scope: 'public_profile,user_friends,email'});
+        }, {scope: 'public_profile,user_friends,email'});*/
+        logMeIn();
       }
     });
   });
@@ -106,4 +108,15 @@ function statusChangeCallback(response) {
       statusChangeCallback(response);
     }, {scope: 'public_profile,user_friends,email'});
   }
+}
+
+function logMeIn()
+{
+  var paramsLocation=window.location.toString().indexOf('?');
+  var params="";
+  if (paramsLocation>=0)
+    params=window.location.toString().slice(paramsLocation);
+
+  top.location = 'https://graph.facebook.com/oauth/authorize?'+
+    'client_id=776597119112980&scope=public_profile,user_friends,email&redirect_uri=http://yumsapp.herokuapp.com/home'+params;
 }

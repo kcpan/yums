965d9be925b78d521f6aa0112754188286b75b29
Kevin Pan <kcpan@ucsd.edu>
2016-03-07 04:41:29 +0000

    restriction stuff half working

--- public/js/restrictions.js
+++ public/js/restrictions.js
@@ -1,48 +1,12 @@
 $(document).ready(function() {
-    var first           = 1;
     var max_fields      = 10; //maximum input boxes allowed
-    var wrapper         = $(".input-fields-wrap"); //Fields wrapper
-    var add_button      = $(".add-field-button"); //Add button ID
-    var count=16;
+    var count=6;
+  
 
-    var x = 1; //initlal text box count
-    $(".add-field-button").click(function(){ //on add input button click
-        event.preventDefault();
-        if(checkInput()){
-          addField();
-          first = 0;
-        }
-        else{
-          alert("Fill in input before adding.");
-        }
-    });
-
-    $(wrapper).on("click",".add-field-button", function(){ //user click on remove text
-        event.preventDefault();
-        if(checkInput()){
-          addField();
-        }
-        else{
-          if(first == 0){
-            alert("Fill in input before adding.");
-          }
-        }
-    })
-
-    $(wrapper).on("click",".remove-field", function(){ //user click on remove text
-        event.preventDefault();
-        $(this).parent('span').parent('div').remove();
-        x--;
-    })
-
-    function addField(){
-      if(x < max_fields){ //max input box allowed
-          x++; //text box increment
-          $(".add-field-button").replaceWith('<button class="remove-field">X</button>');
-          $(wrapper).append('<div class="field-line wow fadeInUp"><span class="text-buf"><input class="res-field" type="text" name="mytext[]"/></span>' +
-                            '<span class="btn-buf"><button class="add-field-button">+</button></span></div>'); //add input box
-      }
-    }
+    // for(var i = 0; i < friendslist.length; i++) {
+    //     var toadd = ' <li><label class="fbfriends checkbox-inline chex"><input type="checkbox" value=""><label>X</label>' + friendslist[i].name + '</label></li>';
+    //     $(".checks").append(toadd);
+    // }
 
     $(".roll").click(function(){
         event.preventDefault();
@@ -63,7 +27,6 @@ $(document).ready(function() {
         $(this).css("background-color", "rgb(207, 75, 75)");
         $(this).css("color", "rgb(255, 255, 255)");
 
-
         var counter=setInterval(timer, 1000); //1000 will  run it every 1 second
 
         function timer()
@@ -80,15 +43,16 @@ $(document).ready(function() {
           //Do code for showing the number of seconds here
            $(".roll").text(count); // watch for spelling
         }
-    })
+    });
 
 
 
     function checkInput(){
       var input = $(".add-field-button").parent().siblings().children().val();
       console.log("input" + input);
-      return $.trim(input)
+      return $.trim(input);
     }
+
 });
 
 function addData(result){

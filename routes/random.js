e3fa1d481cef8982fbde993788428156551d782b
Kevin Pan <kcpan@ucsd.edu>
2016-03-10 23:01:05 +0000

    less restrcits BITCH

--- routes/random.js
+++ routes/random.js
@@ -1,22 +1,10 @@
 // Get all of our friend data
 //var data = require('../data.json');
-var categories = require('../categories.json');
+var less = require('../less.json');
 
 exports.view = function(req, res){
 
-	cat = JSON.stringify(categories);
-	final = {"place":[]};
-	for(i = 0; i < categories.length; i++)
-	{
-		// console.log(categories[i].parents);
-		if(categories[i].parents=='restaurants' || categories[i].parents=='food')
-		{
-			final.place.push({'title':categories[i].title, 'alias':categories[i].alias});
-		}
-	}
-	console.log(final);
-
-	res.render('random', final);
+	res.render('random', less);
 
 	//res.render('index');
 };
